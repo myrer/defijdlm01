7970f7c4d61954ff79e870b2aed119a1bee87917
rick@osboxes <richard.myre@gmail.com>
2016-01-02 15:53:02 -0500

    INitila commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Defijdlm::Application.config.secret_key_base = '7f661c6e5c16a4198490fe8bb44acdc4b51644d514e4e2df02afa2042307518a8fcb06faa6e26c390c57f5b2dc25d5a33d9be13a79056a2608d9fe583df5308f'
